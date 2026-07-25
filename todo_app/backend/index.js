4c7cbcd55c7f8be2ba77d21259ba79b172490a43
syed sufiyan ali <sufiyantdc@gmail.com>
2026-07-25 22:47:27 +0530

    todo backend created

--- todo_app/backend/index.js
+++ todo_app/backend/index.js
@@ -6,11 +6,62 @@ app.use(cors());
 
 let todoArr = ["go to market"];
 
+function updateTodo(oldTodo, newTodo) {
+  let index;
+  let filteredArr = todoArr.filter((todo, index) => {
+    if (todo === oldTodo) {
+      index = index;
+      return false;
+    } else {
+      return true;
+    }
+  });
+  filteredArr.splice(index, 0, newTodo);
+  todoArr = filteredArr;
+}
+
 app.get("/todo", (response, request) => {
   response.json({
     data: todoArr,
   });
 });
-app.post("/todo", (response, request) => {});
-app.put("/todo", (response, request) => {});
-app.delete("/todo", (response, request) => {});
+app.post("/todo", (response, request) => {
+  console.log("post request received on /todo path");
+  const { todo } = request.body;
+
+  todoArr.push(todo);
+  console.log(todoArr);
+
+  response.json({
+    message: "todo added sucessfully",
+  });
+});
+
+app.put("/todo", (response, request) => {
+  const { oldTodo, newTodo } = request.body;
+
+  updateTodo(oldTodo, newTodo);
+
+  response.json({
+    message: "todo updated sucessfully   ",
+  });
+});
+app.delete("/todo", (response, request) => {
+  const { todo } = request.body;
+  todoArr = todoArr.filter((element) => {
+    if (element === todo) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  response.json({
+    message: "todo deleted sucessfully",
+  });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("server running at port: ", PORT);
+});
