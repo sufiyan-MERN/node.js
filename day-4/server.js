5bba56bbf142a350c68ecccd9223d3b7bc24fb20
syed sufiyan ali <sufiyantdc@gmail.com>
2026-07-30 16:03:20 +0530

    multiple middleware added

--- day-4/server.js
+++ day-4/server.js
@@ -1,19 +1,64 @@
 const express = require("express");
 const app = express();
 const restaurantsArr = require("./utlis/mockdata");
+const cors = require("cors");
 
 const addTimeStamps = (req, res, next) => {
   const now = new Date();
   const time = now.toLocaleTimeString();
 
-  console.log("time is", time);
+  //   console.log("time is", time);
 
   req.timeStamps = time;
 
   next();
 };
 
-app.get("/menu/:id", addTimeStamps, (req, res) => {
+const requestLoggerMiddleware = (req, res, next) => {
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+
+  console.log("-----New Request-----");
+  console.log("mehtod is: ", req.method);
+  console.log("Route is: ", req.url);
+  console.log("Time is: ", time);
+  next();
+};
+
+let count = 0;
+const requestCounter = (req, res, next) => {
+  count++;
+  console.log("current request count: ", count);
+  next();
+};
+
+const rateLimiter = (req, res, next) => {
+  if (count < 5) {
+    next();
+  } else {
+    res.status(429).json({
+      msg: "limite exceed",
+    });
+  }
+};
+
+const resNameMddleware = (req, res, next) => {
+  console.log("Name of the restsurant: ");
+  restaurantsArr.map((resname) => {
+    console.log(resname.resName);
+  });
+
+  //   next();
+};
+
+app.use(express.json());
+app.use(cors());
+app.use(addTimeStamps);
+app.use(requestLoggerMiddleware);
+app.use(requestCounter);
+app.use(rateLimiter);
+
+app.get("/menu/:id", resNameMddleware, (req, res) => {
   const { id } = req.params;
 
   const targetMenu = restaurantsArr.filter((restaurant) => {
@@ -25,7 +70,7 @@ app.get("/menu/:id", addTimeStamps, (req, res) => {
   });
 });
 
-app.get("/restaurant", addTimeStamps, (req, res) => {
+app.get("/restaurant", (req, res) => {
   console.log(req.timeStamps);
 
   res.json({
