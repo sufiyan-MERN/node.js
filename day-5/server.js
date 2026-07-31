13845181219a51df2550632a3ba9f1d79673dfaf
syed sufiyan ali <sufiyantdc@gmail.com>
2026-07-31 16:09:56 +0530

    token generator

---
+++ day-5/server.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const { generateKey } = require("node:crypto");
+const app = express();
+app.use(express.json());
+
+let users = [];
+
+const generateToken = () => {
+  let options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let token = "";
+  for (let i = 0; i < 32; i++) {
+    token = token + options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+};
+
+app.post("/singup", (req, res) => {
+  const { username, email, password } = req.body;
+
+  const userObj = {
+    username,
+    email,
+    password,
+  };
+  users.push(userObj);
+  console.log(users);
+
+  res.status(200).json({
+    msg: "user registered sucessfully",
+  });
+});
+
+app.post("/singin", (req, res) => {
+  const { email, password } = req.body;
+  let foundUser = null;
+
+  foundUser = users.find((user) => {
+    if (user.email == email && user.password == password) {
+      return true;
+    }
+  });
+
+  if (foundUser) {
+    const token = generateToken();
+    foundUser.token = token;
+    console.log("db check", users);
+    res.json({
+      mes: "login sucessfully",
+      token: token,
+    });
+  } else {
+    res.json({
+      msg: "invalid email or password",
+    });
+  }
+});
+
+const PORT = "8080";
+app.listen(PORT, () => {
+  console.log("server is running at port: ", PORT);
+});
