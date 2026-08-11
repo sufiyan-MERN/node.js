89fe9c5e4f2880e46a8e36ddbb3ca7e14f7dc232
syed sufiyan ali <sufiyantdc@gmail.com>
2026-08-11 16:16:06 +0530

    todo backend and database completed

--- day-7/server.js
+++ day-7/server.js
@@ -1,5 +1,5 @@
-// const dns=require("dns")
-// dns.setServers(["8.8.8.8", "1.1.1.1"]);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -27,7 +27,7 @@ app.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
 
   const feedback = await UserModel.create({
-    username, 
+    username,
     email,
     password,
   });
@@ -39,88 +39,98 @@ app.post("/signup", async (req, res) => {
 });
 
 app.post("/signin", async (req, res) => {
-  const {email,password}=req.body
-  const foundUser= await UserModel.find({
-    email:email,
-    password:password,
-  })
-  if(foundUser != undefined){
-    const token=jwt.sign({id:foundUser[0]._id.toString()},process.env.JWT_SECRET)
-  
-  res.json({
-    msg:"login successfull",
-    token:token
+  const { email, password } = req.body;
+  const foundUser = await UserModel.find({
+    email: email,
+    password: password,
+  });
+  if (foundUser != undefined) {
+    const token = jwt.sign(
+      { id: foundUser[0]._id.toString() },
+      process.env.JWT_SECRET,
+    );
 
-  })
-}
-  else{
     res.json({
-      msg:"invalid credentials"
-    })
+      msg: "login successfull",
+      token: token,
+    });
+  } else {
+    res.json({
+      msg: "invalid credentials",
+    });
   }
 });
 
-app.use(authMiddleWare)
+app.get("/test", (req, res) => {
+  res.json({
+    msg: "postman testing",
+  });
+});
 
-app.get("/me", async(req, res)=>{
-    const userId = req.userId;
+app.use(authMiddleWare);
 
-    const data = await UserModel.find({_id: userId});
+app.get("/me", async (req, res) => {
+  const userId = req.userId;
 
-    res.json({data})
-})  
+  const data = await UserModel.find({ _id: userId });
+  console.log(data);
+  res.json({ data });
+});
 
 app.get("/todo", async (req, res) => {
-
-  const   userId=req.userId
-  const data=await todoModel.find({
+  const userId = req.userId;
+  const data = await TodoModel.find({
     userId,
-  })
+  });
 
   res.json({
+    data,
     msg: "get request received",
   });
 });
 
-app.post("/todo", async (req,res)=>{
-  const {title,description,isDone}=req.body
-  const feedback=await TotoModel.create({
-    title:title,
+app.post("/todo", async (req, res) => {
+  const { title, description, isDone } = req.body;
+  const feedback = await TodoModel.create({
+    title: title,
     description,
     isDone,
-    userId:req.userId
-  })
+    userId: req.userId,
+  });
 
   res.json({
-    msg:"toto added successfully",
-    feedback
-  })
-})
+    msg: "toto added successfully",
+    feedback,
+  });
+});
 
-app.put("/todo",async (req,res)=>{
-  userId=req.userId
-  const {titile,description,isDone}=req.body
-  const feedback=await TodoModel.findOneAndReplace({
-    title:title,
-    userId:userId
-  },{title,description,isDone,userId},{new:true})
+app.put("/todo", async (req, res) => {
+  userId = req.userId;
+  const { titile, description, isDone } = req.body;
+  const feedback = await TodoModel.findOneAndReplace(
+    {
+      title: title,
+      userId: userId,
+    },
+    { title, description, isDone, userId },
+    { new: true },
+  );
   res.json({
-    msg:"todo updated successfully",
-    feedback
-  })
-})
+    msg: "todo updated successfully",
+    feedback,
+  });
+});
 
-app.delete("/todo",async (req,res) => {
-  const userId=req.userId
-  const title=req.query.title
-  const feedback=await TodoModel.findOneAndDelete({title,userId})
+app.delete("/todo", async (req, res) => {
+  const userId = req.userId;
+  const title = req.query.title;
+  const feedback = await TodoModel.findOneAndDelete({ title, userId });
 
   res.json({
-    msg:"todo deeleted successfully",
-    feedback
-  })
-})
-
+    msg: "todo deeleted successfully",
+    feedback,
+  });
+});
 
 app.listen("8080", () => {
   console.log("server is listening at port 8080");
